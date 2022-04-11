b0c9a38b74859908a9aa3d18e05f61fbf229ca3e
nos64 <manosov1984@gmail.com>
2022-04-11 16:04:03 +0400

    feat: count-cats, carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(num) {
+  let result = 0;
+  let t = 0.693/HALF_LIFE_PERIOD;
+  if (typeof num === 'string') {
+    if (+num > MODERN_ACTIVITY || +num > 9000 || parseInt(num) <= 0 || isNaN(parseInt(num))) return false;
+    else {
+      result = Math.log(MODERN_ACTIVITY / num) / t;
+      return Math.ceil(result, 1)
+    }
+  } 
+  else return false
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
