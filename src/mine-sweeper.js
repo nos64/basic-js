d25ae640a750d7bb9992fd0ad93bae0bb677ae93
nos64 <manosov1984@gmail.com>
2022-04-18 15:47:38 +0400

    feat: minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,7 +23,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
+function minesweeper(matrix) {
+  let result = Array.from(matrix).map(item => item.map(item => item === false ? +item : item))
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      if (result[i][j] === true  && i === 0) {
+        if (j === 0) {
+          result[i][j+1] +=1
+          result[i+1][j] +=1
+        } else if (j === result[i].length - 1) {
+          result[i][j-1] +=1
+          result[i+1][j] +=1
+        } else {
+          result[i][j-1] +=1
+          result[i][j+1] +=1
+          result[i+1][j] +=1
+        }
+      }
+      else if (result[i][j] === true  && i === result.length - 1) {
+        if (j === 0) {
+          result[i][j+1] +=1
+          result[i-1][j] +=1
+        } else if (j === result[i].length - 1) {
+          result[i][j-1] +=1
+          result[i-1][j] +=1
+        } else {
+          result[i][j-1] +=1
+          result[i][j+1] +=1
+          result[i-1][j] +=1
+        }
+      } else if (result[i][j] === true && i !== 0 && i !== result.length - 1) {
+          if (j === 0) {
+            result[i][j+1] +=1
+            result[i-1][j] +=1
+            result[i+1][j] +=1
+          } else if (j === result[i].length - 1) {
+            result[i][j-1] +=1
+            result[i+1][j] +=1
+            result[i-1][j] +=1
+          } else {
+            // result[i-1][j-1] +=1
+            result[i-1][j] +=1
+            result[i-1][j+1] +=1
+            result[i][j-1] +=1
+            result[i][j+1] +=1
+            result[i+1][j-1] +=1
+            result[i+1][j] +=1
+            result[i+1][j+1] +=1
+
+          }
+      }
+  
+    }
+  }
+
+    return result.map(item => item.map(item => item === true ? +item : item))
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
